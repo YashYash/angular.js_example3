cc3a9a9d888cca3a7320e648b66d552c3c84dd1a
YashYash <yash.saxena1217@gmail.com>
2014-03-10 15:20:34 -0700

    rotten tomatoes api

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+// Define a new module. This time we declare a dependency on
+// the ngResource module, so we can work with the Instagram API
+
+var app = angular.module("movieApp", ['ngResource']);
+
+// Create and register the new "instagram" service
+app.factory('rotten_tomatoes', function($resource){
+
+	return {
+		fetchMovie: function(callback){
+
+			var api = $resource("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=:key&q=:movie_title&page_limit=1&callback=JSON_CALLBACK",{
+				movie_title:'batman',
+				key:'7tbrsn6cqasdwjx96a4py2ft'
+			},{
+				// This creates an action which we've chosen to name "fetch". It issues
+				// an JSONP request to the URL of the resource. JSONP requires that the
+				// callback=JSON_CALLBACK part is added to the URL.
+				fetch:{method:'JSONP'}
+			});
+		
+			api.fetch(function(response){
+				console.log(response);
+				// Call the supplied callback function
+				callback(response);
+			});
+		}
+		
+	}
+});
+
+
---
+++ js/movieController.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+app.controller("movieController", function($scope, rotten_tomatoes){
+	rotten_tomatoes.fetchMovie(function(data){
+
+	});
+});
